f315bdda098fe1dc568f779f9134a9059d478fb3
Haoju <haoju.xiao@rapyuta-robotics.com>
2023-05-07 12:30:30 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,12 @@
 vim.wo.number = true
 
+vim.g.mapleader = ' '
+
 require('plugins')
 
 local map = vim.api.nvim_set_keymap
-
 map('n', 'n', [[:NvimTreeToggle<CR>]], {})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
