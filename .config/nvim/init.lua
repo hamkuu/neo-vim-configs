b895ef2d7d2c88b9403f6d6a7d4cbcc5e23a7f5c
Haoju <haoju.xiao@rapyuta-robotics.com>
2023-05-07 18:49:25 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,47 @@
 vim.wo.number = true
 
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.breakindent = true
+
+vim.cmd[[colorscheme tokyonight-moon]]
 
 require('plugins')
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { "lua_ls", "pylsp" }
+})
+require('lspconfig').lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+require('lspconfig').pylsp.setup {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
 
 local map = vim.api.nvim_set_keymap
 map('n', 'n', [[:NvimTreeToggle<CR>]], {})
