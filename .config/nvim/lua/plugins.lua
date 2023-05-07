6cad55fa0e41f87eac4c9457d7f0a5c744c11192
Haoju <haoju.xiao@rapyuta-robotics.com>
2023-05-07 18:49:44 +0900

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,10 +2,21 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use 'folke/tokyonight.nvim'
+
   use {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require("nvim-tree").setup {}
     end
   }
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig'
 end)
